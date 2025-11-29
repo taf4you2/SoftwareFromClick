8a5c6ea27cb6edea468a2a1aad954b90ba1ab6f7
wojtek <wojtek@DESKTOP-MSN5DM6>
2025-11-29 01:47:55 +0100

    wprowadzono male poprawki w mainwindow i zrobiono settingswindow
    ide spac

---
+++ SoftwareFromClick/Views/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace SoftwareFromClick.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.ShowDialog();
+            //settingsWindow.Show(); 
+            /*
+                roznica jest kosmetyczna a jednak fundamentalna
+                bo w tym pierwszym nie mozna kilikac w glownym oknie w nic a w drugim mozna
+                i jest to ciekawe
+             */
+
+        }
+    }
+}
---
+++ SoftwareFromClick/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SoftwareFromClick.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Tutaj kod zapisywania do bazy
+
+            this.DialogResult = true; // Zwraca true do MainWindow
+            this.Close(); // Zamyka okno
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false; // Zwraca false do MainWindow
+            this.Close(); // Zamyka okno bez zapisywania
+        }
+
+    }
+}
