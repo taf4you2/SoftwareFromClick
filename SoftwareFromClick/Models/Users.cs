724e69475a56f7b82b025b3d8f816bd1d213319c
wojtek <wojtek@DESKTOP-MSN5DM6>
2025-12-01 13:10:50 +0100

    dodano pliki w celu utworzenia encji

---
+++ SoftwareFromClick/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace SoftwareFromClick.Models
+{
+    public class Users
+    {
+        // Klucz główny
+        public int Id { get; set; }
+
+        // Dane użytkownika
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
